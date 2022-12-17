601a062152003de90d66da30813e0c366bed7060
Najlaalrajhia98 <najla.alrajhi@tra.gov.om>
2022-12-17 18:36:28 +0400

    added a file array of numbers repeated

---
+++ src/ArrayOfNumRepeated.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+
+/**
+ * 
+ */
+
+/**
+ * @author LAP-7 
+ *
+ */
+public class ArrayOfNumRepeated {
+
+    /**
+     * A class uses Hashmap to find how many times the integer elements occured inside the array 
+     */
+            public static void main(String[] args) {
+
+                 // array elements will act as a keys
+                // the value of hashmap is the num of repetation 
+                
+                int arr[]= {1,1,2,6,5,3,2,6,7,1,3,2,9,9}; 
+                HashMap<Integer,Integer> numoccurance= new HashMap<>(); 
+                
+                for (int i=0;i<arr.length;i++) 
+                {   // in the begining the map will not contain anaything so if the value does not contained 
+                    //then put it with number of occurance equal to 1 initially 
+                    if (!numoccurance.containsKey(arr[i])) 
+                    {
+                        numoccurance.put(arr[i], 1);
+
+
+                    } 
+                    else 
+                    {
+                        numoccurance.put(arr[i], numoccurance.get(arr[i])+1);
+                       
+                    }
+
+                } 
+                System.out.println(" EACH NUMBER  =   TIMES OF OCCURANCE" );
+                System.out.println("--------------------------------" );
+                System.out.print(numoccurance);
+
+
+
+
+
+    }
+
+}
