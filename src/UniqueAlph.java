44e6391342520223976f9776df6c089d0a6bdde3
Najlaalrajhia98 <najla.alrajhi@tra.gov.om>
2022-12-20 11:53:09 +0400

    UniqueAlpha using hashMap

---
+++ src/UniqueAlph.java
@@ -0,0 +1,43 @@
+import java.security.PublicKey;
+import java.util.HashMap;
+
+/**
+ * 
+ */
+
+/**
+ * @author LAP-7
+ *
+ */
+public class UniqueAlph {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // using hashMap
+     char  arr [] = { 'c' , 'a' , 'c' , 't' , 'a' , 'r' , 'a' , 't' , 'z' };
+     
+    HashMap<Character, Integer> a =hashMap(arr);
+     System.out.print(a.keySet());
+    }
+    
+    
+   public static  HashMap<Character, Integer> hashMap (char [] arr){
+       
+       HashMap<Character,Integer> num= new HashMap<>();
+   
+       for (int i=0;i<arr.length;i++) 
+       {   
+           
+               num.put(arr[i], 1);
+           
+
+          
+      }
+    return num; 
+      
+  }
+
+    }
+    
