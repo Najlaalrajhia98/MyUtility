c42d1ff628f27c867a6702a2f2ac36c5d902abf7
Najlaalrajhia98 <najla.alrajhi@tra.gov.om>
2022-12-26 18:49:22 +0400

    using for loop, already done recursivly

---
+++ src/IncremeantDecrment.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author LAP-7
+ *
+ */
+public class IncremeantDecrment {
+
+    /** this class is used to genrate incremeant and decreament seiers of number using for loops.
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        int j=0;
+        for (int i=0; i<=4 ; i++) 
+        {
+            System.out.println(i);
+        }
+        for (j=4;j>=0 ;j--) 
+        {
+            System.out.println(j);
+
+        }
+    }
+
+}
